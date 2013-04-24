c416a1e47089e3aa9ba7608d7bf5e01251a28084
Vincent Pelletier <vincent@nexedi.com>
2013-04-24 20:34:24 +0200

    Add some demos.

---
+++ demo/embedded.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import threading
+import pprofile
+import time
+import sys
+
+def func():
+  # Busy loop, so context switches happe, so context switches happenn
+  end = time.time() + 1
+  while time.time() < end:
+    pass
+
+# Single-treaded run
+prof = pprofile.Profile()
+with prof:
+  func()
+prof.annotate(sys.stdout, __file__)
+
+# Dual-threaded run
+t1 = threading.Thread(target=func)
+prof = pprofile.Profile()
+with prof:
+  t1.start()
+  func()
+  t1.join()
+prof.annotate(sys.stdout, __file__)
---
+++ demo/threads.py
@@ -0,0 +1,16 @@
+import threading
+import time
+
+def func():
+  time.sleep(1)
+
+def func2():
+  pass
+
+t1 = threading.Thread(target=func)
+t2 = threading.Thread(target=func)
+t1.start()
+t2.start()
+(func(), func2())
+t1.join()
+t2.join()
