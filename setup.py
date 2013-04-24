7f21f9de8421c8ae1a84e44c79bfbf512c2396ce
Vincent Pelletier <vincent@nexedi.com>
2013-04-24 21:10:29 +0200

    Bump to 1.2 .

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ description = open(join(dirname(__file__), 'README.rst')).read()
 
 setup(
     name='pprofile',
-    version='1.1',
+    version='1.2',
     author='Vincent Pelletier',
     author_email='plr.vincent@gmail.com',
     description=next(x for x in description.splitlines() if x.strip()),
