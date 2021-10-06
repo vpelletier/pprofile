vpelletier/pprofile

commit 5a3e8ffacaad90d14f115fb87e0cbfef98d2d6fb

pprofile: Move cachegrind archive generation methods from pprofile.zope .

pprofile/zope.py
@@ -79,18 +79,12 @@ Example statistic usage (to profile other running threads):
 """
 from __future__ import print_function
 import dis
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from email.encoders import encode_quopri
 import functools
 import gc
 from io import StringIO, BytesIO
 from importlib import import_module
 import itertools
-import os
 from collections import defaultdict
-import zipfile
 import pprofile
 
 def getFuncCodeOrNone(module, attribute_path):
@@ -127,7 +121,6 @@ else:
     functools.update_wrapper(unrestrictedTraverse_spy, orig_unrestrictedTraverse)
     OFS.Traversable.Traversable.unrestrictedTraverse = unrestrictedTraverse_spy
 
-_ALLSEP = os.sep + (os.altsep or '')
 PYTHON_EXPR_FUNC_CODE_SET = (ZRPythonExpr__call__func_code, PythonExpr__call__func_code)
 
 class ZopeFileTiming(pprofile.FileTiming):
@@ -409,25 +402,8 @@ class ZopeMixIn(object):
         Yields path, data, mimetype for each file involved on or produced by
         profiling.
         """
-        out = StringIO()
-        self.callgrind(out, relative_path=True)
-        yield (
-            'cachegrind.out.pprofile',
-            out.getvalue(),
-            'application/x-kcachegrind',
-        )
-        for name, lines in self.iterSource():
-            lines = ''.join(lines)
-            if lines:
-                if isinstance(lines, unicode):
-                    lines = lines.encode('utf-8')
-                yield (
-                    os.path.normpath(
-                        os.path.splitdrive(name)[1]
-                    ).lstrip(_ALLSEP),
-                    lines,
-                    'text/x-python',
-                )
+        for entry in super(ZopeMixIn, self)._iterOutFiles():
+            yield entry
         sql_name_template = 'query_%%0%ii-%%i_hits_%%6fs.sql' % len(
             str(len(self.sql_dict)),
         )
@@ -485,7 +461,7 @@ class ZopeMixIn(object):
                 'text/plain',
             )
 
-    def asMIMEString(self):
+    def getCallgrindMIME(self):
         """
         Return a mime-multipart representation of:
         - callgrind profiling statistics (cachegrind.out.pprofile)
@@ -495,37 +471,17 @@ class ZopeMixIn(object):
           (unrestrictedTraverse_pathlist.txt)
         - all involved python code, including Python Scripts without hierarchy
           (the rest)
+        and the mimetype of this string.
         To unpack resulting file, see "unpack a MIME message" in
           http://docs.python.org/2/library/email-examples.html
         Or get demultipart from
           https://pypi.python.org/pypi/demultipart
         """
-        result = MIMEMultipart()
-        base_type_dict = {
-            'application': MIMEApplication,
-            'text': MIMEText,
-        }
-        encoder_dict = {
-            'application/x-kcachegrind': encode_quopri,
-            'text/x-python': 'utf-8',
-            'text/plain': 'utf-8',
-        }
-        for path, data, mimetype in self._iterOutFiles():
-            base_type, sub_type = mimetype.split('/')
-            chunk = base_type_dict[base_type](
-                data,
-                sub_type,
-                encoder_dict.get(mimetype),
-            )
-            chunk.add_header(
-                'Content-Disposition',
-                'attachment',
-                filename=path,
-            )
-            result.attach(chunk)
-        return result.as_string(), result['content-type']
+        out = BytesIO()
+        mimetype = super(ZopeMixIn, self).getCallgrindMIME(out)
+        return out.getvalue(), mimetype
 
-    def asZip(self):
+    def getCallgrindZip(self):
         """
         Return a serialised zip archive containing:
         - callgrind profiling statistics (cachegrind.out.pprofile)
@@ -535,16 +491,15 @@ class ZopeMixIn(object):
           (unrestrictedTraverse_pathlist.txt)
         - all involved python code, including Python Scripts without hierarchy
           (the rest)
+        and the "application/zip" mimetype.
         """
         out = BytesIO()
-        with zipfile.ZipFile(
-            out,
-            mode='w',
-            compression=zipfile.ZIP_DEFLATED,
-        ) as outfile:
-            for path, data, _ in self._iterOutFiles():
-                outfile.writestr(path, data)
-        return out.getvalue(), 'application/zip'
+        mimetype = super(ZopeMixIn, self).getCallgrindZip(out)
+        return out.getvalue(), mimetype
+
+    # BBB
+    asMIMEString = getCallgrindMIME
+    asZip = getCallgrindZip
 
 class ZopeProfiler(ZopeMixIn, pprofile.Profile):
     __slots__ = ZopeMixIn.virtual__slots__

.travis.yml
@@ -34,3 +34,5 @@ script:
   - pprofile --include demo demo/twocalls2.py
   - pprofile --include demo demo/the_main.py
   - pprofile --include demo demo/module_globals.py
+  - pprofile --format callgrindzip --out test_threads.zip demo/threads.py && unzip -l test_threads.zip
+  - pprofile --out test_threads.zip demo/threads.py && unzip -l test_threads.zip

pprofile/__init__.py
@@ -41,6 +41,10 @@ Statistic profiling:
 """
 from __future__ import print_function, division
 from collections import defaultdict, deque
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.encoders import encode_quopri
 from functools import partial, wraps
 # Note: use time, not clock.
 # Clock, at least on linux, ignores time not spent executing code
@@ -507,6 +511,103 @@ class ProfileBase(object):
                     key=lambda x: file_dict[x].getSortKey()
                 )
         return filename
+
+    def _iterOutFiles(self, filename=None, commandline=None):
+        """
+        Yields path, data, mimetype for each file involved on or produced by
+        profiling.
+        """
+        out = io.StringIO()
+        self.callgrind(
+            out,
+            filename=filename,
+            commandline=commandline,
+            relative_path=True,
+        )
+        yield (
+            'cachegrind.out.pprofile',
+            out.getvalue(),
+            'application/x-kcachegrind',
+        )
+        for name in self._getFileNameList(filename, may_sort=False):
+            lines = ''.join(self._iterRawFile(name))
+            if lines:
+                if isinstance(lines, unicode):
+                    lines = lines.encode('utf-8')
+                yield (
+                    _relpath(name),
+                    lines,
+                    'text/x-python',
+                )
+
+    def getCallgrindMIME(self, out, filename=None, commandline=None, relative_path=True):
+        """
+        Write to "out" a mime-multipart representation of:
+        - callgrind profiling statistics (cachegrind.out.pprofile)
+        - all involved python code, including Python Scripts without hierarchy
+          (the rest)
+        and return its mimetype.
+        To unpack resulting file, see "unpack a MIME message" in
+          http://docs.python.org/2/library/email-examples.html
+        Or get demultipart from
+          https://pypi.python.org/pypi/demultipart
+
+        relative_path:
+          Ignored.
+        """
+        _ = relative_path
+        result = MIMEMultipart()
+        base_type_dict = {
+            'application': MIMEApplication,
+            'text': MIMEText,
+        }
+        encoder_dict = {
+            'application/x-kcachegrind': encode_quopri,
+            'text/x-python': 'utf-8',
+            'text/plain': 'utf-8',
+        }
+        for path, data, mimetype in self._iterOutFiles(
+            filename=filename,
+            commandline=commandline,
+        ):
+            base_type, sub_type = mimetype.split('/')
+            chunk = base_type_dict[base_type](
+                data,
+                sub_type,
+                encoder_dict.get(mimetype),
+            )
+            chunk.add_header(
+                'Content-Disposition',
+                'attachment',
+                filename=path,
+            )
+            result.attach(chunk)
+        out.write(result.as_string())
+        return result['content-type']
+
+    def getCallgrindZip(self, out=None, filename=None, commandline=None, relative_path=True):
+        """
+        Write to "out" a serialised zip archive containing:
+        - callgrind profiling statistics (cachegrind.out.pprofile)
+        - all involved python code, including Python Scripts without hierarchy
+          (the rest)
+        as a byte string and the "application/zip" mimetype.
+
+        relative_path:
+          Ignored.
+        """
+        _ = relative_path
+        with zipfile.ZipFile(
+            out,
+            mode='w',
+            compression=zipfile.ZIP_DEFLATED,
+        ) as outfile:
+            for path, data, _ in self._iterOutFiles(
+                filename=filename,
+                commandline=commandline,
+            ):
+                outfile.writestr(path, data)
+        return 'application/zip'
 
     def callgrind(self, out, filename=None, commandline=None, relative_path=False):
         """
@@ -1232,6 +1333,7 @@ def _main(argv, stdin=None):
     format_dict = {
         'text': 'annotate',
         'callgrind': 'callgrind',
+        'callgrindzip': 'getCallgrindZip',
     }
 
     parser = argparse.ArgumentParser(argv[0])
@@ -1325,7 +1427,9 @@ def _main(argv, stdin=None):
         }
         runner_method_id = 'runmodule'
     if options.format is None:
-        if _isCallgrindName(options.out):
+        if os.path.splitext(options.out)[1] == os.path.extsep + 'zip':
+            options.format = 'callgrindzip'
+        elif _isCallgrindName(options.out):
             options.format = 'callgrind'
         else:
             options.format = 'text'
@@ -1350,7 +1454,10 @@ def _main(argv, stdin=None):
             out = EncodeOrReplaceWriter(sys.stdout)
             close = lambda: None
         else:
-            out = io.open(options.out, 'w', errors='replace')
+            if options.format == 'callgrindzip':
+                out = io.open(options.out, 'wb')
+            else:
+                out = io.open(options.out, 'w', errors='replace')
             close = out.close
         if options.exclude:
             exclusion_search_list = [

commit 3988837aca1e5f62e245be644f124afb53f64327

pprofile.zope: Fix _iterOutFiles signature.
Fixes a regression introduced in 5a3e8ffacaad90d14f115fb87e0cbfef98d2d6fb:
  pprofile: Move cachegrind archive generation methods from pprofile.zope .

pprofile/zope.py
@@ -397,12 +397,12 @@ class ZopeMixIn(object):
             self.keep_alive.append(frame_globals)
             return file_timing
 
-    def _iterOutFiles(self):
+    def _iterOutFiles(self, *args, **kw):
         """
         Yields path, data, mimetype for each file involved on or produced by
         profiling.
         """
-        for entry in super(ZopeMixIn, self)._iterOutFiles():
+        for entry in super(ZopeMixIn, self)._iterOutFiles(*args, **kw):
             yield entry
         sql_name_template = 'query_%%0%ii-%%i_hits_%%6fs.sql' % len(
             str(len(self.sql_dict)),